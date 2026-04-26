3df8f58df14e95289d3d86169f55b697fbaeb228
Maksim <github073108@gmail.com>
2026-04-26 15:21:16 +0300

    feat: register Book model in admin

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Book
 
 
 @admin.register(Author)
@@ -8,3 +8,12 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display_links = ['name']
     search_fields = ['name']
     ordering = ['name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['title']
+    list_filter = ['author']
+    search_fields = ['title']
+    ordering = ['title']
