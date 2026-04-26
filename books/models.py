2976f21975590f6d1398f82fb7f762efec97ab97
Maksim <github073108@gmail.com>
2026-04-26 14:15:40 +0300

    feat: add Book model

--- books/models.py
+++ books/models.py
@@ -11,3 +11,16 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
