9002e47951fde0de0af9fcd75727c28cdc4ba9fb
Maksim <github073108@gmail.com>
2026-04-26 14:09:29 +0300

    feat: add Author model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
